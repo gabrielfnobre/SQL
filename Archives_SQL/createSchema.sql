d18bd304deb99483d9f500ced7261f861c8dd43a
Gabriel-F-Nobre <nobrecoder@gmail.com>
2024-03-25 06:17:10 -0300

    25/03/2024

--- Archives_SQL/createSchema.sql
+++ Archives_SQL/createSchema.sql
@@ -1,1 +1,11 @@
-create schema wm;
+-- CREATE SCHEMA
+
+create schema wm;   -- Esse comando define que um novo esquema deverá ser criado.
+                    -- Um schema em um SGBD é como chamamos o banco de dados em 
+                    -- si.
+
+                    -- É o schema que vai conter todos dos dados que interessam 
+                    -- ao nosso BD, como: tabelas, estrutura do esquema, infor-
+                    -- mações importantes como: quem é a chave primária de uma
+                    -- tabela, quem é a chave estrangeira, como são os relacio-
+                    -- namentos e etc.
