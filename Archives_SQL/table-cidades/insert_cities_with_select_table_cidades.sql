d0411b429ef51b240bb89b902e4a7b70c92a87f0
Gabriel-F-Nobre <nobrecoder@gmail.com>
2024-04-02 10:05:23 -0300

    02/04/2024

--- Archives_SQL/table-cidades/insert_cities_with_select_table_cidades.sql
+++ Archives_SQL/table-cidades/insert_cities_with_select_table_cidades.sql
@@ -1,13 +1,31 @@
-INSERT INTO cidades (nome, area, estado_id)
-VALUES('Campinas', 795, 31);
 
-INSERT INTO cidades (nome, area, estado_id)
-VALUES('Niterói', 133.9, (SELECT id FROM estados WHERE sigla = 'RJ')),
-    ('Caruaru', 920.6, (SELECT id FROM estados WHERE sigla = 'PE')), 
-    ('Juazeiro do Norte', 248.2, (SELECT id FROM estados WHERE sigla = 'CE'));
+-- INSERIR CIDADES USANDO SELECT
 
-UPDATE cidades SET estado_id = (SELECT id FROM estados WHERE sigla = 'SP')
-WHERE nome = 'Campinas';
+-- Aqui temos um exemplo de como usar o select durante a inserção, o uso de select é essencial para
+-- quando desejamos fazer inserções em colunas que usam chave estrangeira. Veja alguns exemplos...
+
+INSERT INTO cidades (nome, area, estado_id) -- Aqui temo um exemplo de inserção comum, note que es-
+VALUES('Campinas', 795, 31);                -- estamos fazendo uma inserção de chave estrangeira em
+                                            -- "estado_id", note que inserimos o valor de id errado
+                                            -- o id correto deveria ser o 44, mas erroneamente in-
+                                            -- serimos o 31, isso ilustra como pode ser errado in-
+                                            -- serir chave estrangeira na mão.
+
+INSERT INTO cidades (nome, area, estado_id)                                     -- Aqui já temos um 
+VALUES('Niterói', 133.9, (SELECT id FROM estados WHERE sigla = 'RJ')),          -- exemplo usando o
+    ('Caruaru', 920.6, (SELECT id FROM estados WHERE sigla = 'PE')),            -- select para bus-
+    ('Juazeiro do Norte', 248.2, (SELECT id FROM estados WHERE sigla = 'CE'));  -- car os estados 
+                                                                                -- pelas siglas, o 
+                                                                                -- que é bem mais 
+                                                                                -- fácil e evita 
+                                                                                -- erros de chave
+                                                                                -- estrangeira.
+
+UPDATE cidades SET estado_id = (SELECT id FROM estados WHERE sigla = 'SP')  -- Aqui fizemos o con-
+WHERE nome = 'Campinas';                                                    -- serto usando o se-
+                                                                            -- lect para buscar a
+                                                                            -- chave estrangeira
+                                                                            -- pela sigla...
 
 select * from `cidades`;
 select * from estados;
